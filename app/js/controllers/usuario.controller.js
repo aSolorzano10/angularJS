61c2bf96a825495958e1dedae955ce4e4b56243c
alfredo_solorzano <asolorzanoaguilar@yahoo.com>
2020-06-21 16:46:22 -0500

    implementation ng-bind-template directive

--- app/js/controllers/usuario.controller.js
+++ app/js/controllers/usuario.controller.js
@@ -3,6 +3,5 @@ app.controller('UsuarioController', UsuarioController);
 UsuarioController.$inject=["$scope"]
 
 function UsuarioController($scope){
-  $scope.myHTML= '<p>Términos y condiciones paa el uso del módulo de <strong>Usuarios</strong></p>'
-  + ' para más información visite <a href="http<://angularjs.org/">AngularJS!</a>';
+  $scope.myHTML= 'Términos y condiciones para el uso del módulo de: ';
 }

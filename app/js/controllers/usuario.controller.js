33e2c1b9f7fd43c248500663c0288a665b111536
alfredo_solorzano <asolorzanoaguilar@yahoo.com>
2020-06-21 16:38:10 -0500

    Added angular-sanitize.js file and implement ngBindHtml

--- app/js/controllers/usuario.controller.js
+++ app/js/controllers/usuario.controller.js
@@ -3,5 +3,6 @@ app.controller('UsuarioController', UsuarioController);
 UsuarioController.$inject=["$scope"]
 
 function UsuarioController($scope){
-  $scope.name='MiNombre';
+  $scope.myHTML= '<p>Términos y condiciones paa el uso del módulo de <strong>Usuarios</strong></p>'
+  + ' para más información visite <a href="http<://angularjs.org/">AngularJS!</a>';
 }
